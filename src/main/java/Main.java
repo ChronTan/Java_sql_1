c407e9da26a1c9262fc89eaf4dd8f55ef18823f5
NoName <skarr.age@mail.ru>
2019-05-16 15:54:58 +0300

    Start stage

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        final String user="postgres";
+        final String password="postgres";
+        final String url="jdbc:postgresql://localhost:5432/phones_shop";
+
+        final Connection connection=DriverManager.getConnection(url,user,password);
+
+        Statement st=connection.createStatement();
+        ResultSet rs;
+        
+
+
+
+
+//        try(PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE login = (?)")){
+        try{
+            rs=st.executeQuery("SELECT * FROM users WHERE login != 'Misha'");
+//            statement.setString(1,"Egor");
+//            final ResultSet resultSet=statement.executeQuery();
+            while(rs.next()){
+                String byname="login: "+ rs.getString("login");
+                String byIndex="password: " + rs.getString(3);
+                String byRole="role: "+rs.getInt("role");
+                System.out.println(byname);
+                System.out.println(byIndex);
+                System.out.println(byRole);
+            }
+
+
+        }finally {
+            connection.close();
+        }
+
+    }
+}
