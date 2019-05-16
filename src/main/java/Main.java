811d5fa078bb50e8c3dd9864575f0f29c4ffe1c7
NoName <skarr.age@mail.ru>
2019-05-16 16:18:45 +0300

    back to lesson 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,21 +10,21 @@ public class Main {
 
         final Connection connection=DriverManager.getConnection(url,user,password);
 
-        Statement st=connection.createStatement();
-        ResultSet rs;
+//        Statement st=connection.createStatement();
+//        ResultSet rs;
 
 
-        st.executeQuery("DELETE FROM users WHERE role=4 ");
+//        st.execute("DELETE FROM roles WHERE id>2 ");
 
-//        try(PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE login = (?)")){
-        try{
-            rs=st.executeQuery("SELECT * FROM users WHERE login != 'Misha'");
-//            statement.setString(1,"Egor");
-//            final ResultSet resultSet=statement.executeQuery();
-            while(rs.next()){
-                String byname="login: "+ rs.getString("login");
-                String byIndex="password: " + rs.getString(3);
-                String byRole="role: "+rs.getInt("role");
+        try(PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE id = (?)")){
+//        try{
+//            rs=st.executeQuery("SELECT * FROM users WHERE login != 'Misha'");
+            statement.setInt(1,1);
+            final ResultSet resultSet=statement.executeQuery();
+            if(resultSet.next()){
+                String byname="login: "+ resultSet.getString("login");
+                String byIndex="password: " + resultSet.getString(3);
+                String byRole="role: "+resultSet.getInt("role");
                 System.out.println(byname);
                 System.out.println(byIndex);
                 System.out.println(byRole);
